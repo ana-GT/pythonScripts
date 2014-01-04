a467f5db51a89f77e52c53f993542abff73fea2c
Ana Huaman <ahuaman3@gatech.edu>
2014-01-04 13:48:11 -0500

    Added ex1 for LPTHW

---
+++ LPTHW/ex1.py
@@ -0,0 +1,10 @@
+# *****************************
+# ex1.py: A Good First Program
+# *****************************
+print "Hello world!"
+print "Hello again"
+print "I like typing this (I don't)"
+print "This is fun(it is not)"
+print 'Yay! Printing (why the hell do you yay me?)'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
